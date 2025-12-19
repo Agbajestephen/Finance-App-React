fa85e305c5c2ee6492a7d1e0abef9a28b29eb08d
Agbajestephen <agbajestephen5@gmail.com>
2025-12-19 22:21:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import MainLayout from "./components/MainLayout.jsx";
 import DashboardLayout from "./components/DashboardLayout.jsx";
 
 
+import Dashboard from "./pages/Dashboard.jsx";
+import Investment from "./pages/Investment.jsx";
+import Accounts from "./pages/Accounts.jsx";
+import Transactions from "./pages/Transactions.jsx";
+import CreditCards from "./pages/CreditCards.jsx";
+import Loans from "./pages/Loans.jsx";
+import Services from "./pages/Services.jsx";
+import MyPrivileges from "./pages/MyPrivileges.jsx";
+import Setting from "./pages/Setting.jsx";
+import Signup from "./pages/Signup.jsx";
+
 function App() {
   return (
     <Router>
