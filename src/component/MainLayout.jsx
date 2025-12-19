cbdd40fa0a6fbed0dd3b245c8414b0429ccc7bb5
Agbajestephen <agbajestephen5@gmail.com>
2025-12-19 16:47:56 +0100

    Create MainLayout.jsx

---
+++ src/component/MainLayout.jsx
@@ -0,0 +1,20 @@
+// MainLayout.jsx
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ThemeToggle from "./ThemeToggle.jsx";
+
+const MainLayout = ({ children }) => {
+  const location = useLocation();
+
+  // Hide ThemeToggle on Welcome page
+  const hideToggle = location.pathname === "/";
+
+  return (
+    <>
+      {!hideToggle && <ThemeToggle />}
+      {children}
+    </>
+  );
+};
+
+export default MainLayout;
