2f69518a5f9c54e3b7deed3c82240188f04f030c
Agbajestephen <agbajestephen5@gmail.com>
2025-12-19 16:56:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Welcome from './pages/Welcome.jsx';
-import Login from './pages/Login.jsx';
-import SignUp from './pages/SignUp.jsx';
-import ThemeToggle from './component/ThemeToggle.jsx';
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Welcome from "./pages/Welcome.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import ThemeToggle from "./component/ThemeToggle.jsx";
+import MainLayout from "./component/MainLayout.jsx";
 function App() {
   return (
     <Router>
-      {/* A simple navigation bar for demonstration */}
-      {/* <nav className="navbar bg-base-300 justify-center px-4">
+      <MainLayout>
+        {/* A simple navigation bar for demonstration */}
+        {/* <nav className="navbar bg-base-300 justify-center px-4">
         <div className="flex space-x-4">
           <Link to="/" className="btn btn-ghost">Welcome</Link>
           <Link to="/login" className="btn btn-ghost">Login</Link>
           <Link to="/signup" className="btn btn-ghost">Sign Up</Link>
         </div>
       </nav> */}
-      <div>
-        <ThemeToggle />
-      </div>
 
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+        <div>
+          <ThemeToggle />
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </MainLayout>
     </Router>
   );
 }
