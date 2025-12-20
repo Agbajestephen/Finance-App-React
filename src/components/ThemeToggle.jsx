670a8259efe487d5e67003bbeec6f60f0ce4c7a3
Agbajestephen <agbajestephen5@gmail.com>
2025-12-20 23:27:02 +0100

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -7,7 +7,6 @@ export default function ThemeToggle() {
   );
 
   useEffect(() => {
-    if (newTheme === 'dark') {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
