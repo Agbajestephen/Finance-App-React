a40ae6586bd7472404dbf3f6f30519e21f07ff45
Agbajestephen <agbajestephen5@gmail.com>
2025-12-21 17:54:35 +0100

    Update DashboardLayout.jsx

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Search, Bell, User, Menu, X } from "lucide-react";
-import { Outlet, Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle.jsx";
 function DashboardLayout() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
