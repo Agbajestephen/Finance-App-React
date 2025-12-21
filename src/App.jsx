7f3babd53812a9f1524e7444885987b28472b6c4
ASTROL360 <Garbatoyin360@gmail.com>
2025-12-21 23:03:58 +0100

    lll

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./components/DashboardLayout.jsx";
 
 import Welcome from "./pages/Welcome.jsx";
-import ForgotPassword from "./pages/ForgetPassword.jsx";
+// import ForgotPassword from "./pages/ForgetPassword.jsx";
 import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Investment from "./pages/Investment.jsx";
