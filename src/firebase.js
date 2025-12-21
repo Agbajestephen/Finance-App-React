62a81433f8352cad514c4dc95f863f27af1da41c
Agbajestephen <agbajestephen5@gmail.com>
2025-12-21 20:41:09 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database"; // ✅ use Realtime Database
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
