822249ff6c73212e7129bfdd37621c0e9af64a5d
Agbajestephen <agbajestephen5@gmail.com>
2025-12-21 20:40:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import DashboardLayout from "./components/DashboardLayout.jsx";
 
 import Welcome from "./pages/Welcome.jsx";
