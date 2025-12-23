94c88309f1c626e00cf48df3f7d58451d99b9937
Agbajestephen <agbajestephen5@gmail.com>
2025-12-23 11:20:24 +0100

    Update useAuth.jsx

--- src/state/useAuth.jsx
+++ src/state/useAuth.jsx
@@ -1,46 +1,46 @@
 
-// import { useEffect, useState } from "react";
-// // import { onAuthStateChanged } from "firebase/auth";
-// // import { auth, db } from "../firebase";
-// // import { doc, getDoc } from "firebase/firestore";
+// // import { useEffect, useState } from "react";
+// // // import { onAuthStateChanged } from "firebase/auth";
+// // // import { auth, db } from "../firebase";
+// // // import { doc, getDoc } from "firebase/firestore";
 
 
 
 
-// export function useAuth() {
-//   const [user, setUser] = useState(null);
-//   const [role, setRole] = useState("user");
-//   const [loading, setLoading] = useState(true);
+// // export function useAuth() {
+// //   const [user, setUser] = useState(null);
+// //   const [role, setRole] = useState("user");
+// //   const [loading, setLoading] = useState(true);
 
-//   useEffect(() => {
-//     const unsub = onAuthStateChanged(auth, async (u) => {
-//       setUser(u);
-//       if (u) {
-//         const snap = await getDoc(doc(db, "users", u.uid));
-//         if (snap.exists()) setRole(snap.data().role || "user");
-//       } else {
-//         setRole("user");
-//       }
-//       setLoading(false);
-//     });
-//     return () => unsub();
-//   }, []);
+// //   useEffect(() => {
+// //     const unsub = onAuthStateChanged(auth, async (u) => {
+// //       setUser(u);
+// //       if (u) {
+// //         const snap = await getDoc(doc(db, "users", u.uid));
+// //         if (snap.exists()) setRole(snap.data().role || "user");
+// //       } else {
+// //         setRole("user");
+// //       }
+// //       setLoading(false);
+// //     });
+// //     return () => unsub();
+// //   }, []);
 
-//   return {
+// //   return {
    
+// //     user: { uid: "demo-user", name: "Stephen" },
+// //     role: "admin", // or "user"
+// //     loading: false,
+// //   };
+// // }
+
+
+
+// // src/state/useAuth.jsx
+// export function useAuth() {
+//   return {
 //     user: { uid: "demo-user", name: "Stephen" },
 //     role: "admin", // or "user"
 //     loading: false,
 //   };
 // }
-
-
-
-// src/state/useAuth.jsx
-export function useAuth() {
-  return {
-    user: { uid: "demo-user", name: "Stephen" },
-    role: "admin", // or "user"
-    loading: false,
-  };
-}
