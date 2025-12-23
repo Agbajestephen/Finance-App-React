e9a16a4c07a49966762e2a6a44045350275ff92c
Agbajestephen <agbajestephen5@gmail.com>
2025-12-24 00:15:26 +0100

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import toast from 'react-hot-toast';
 import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
