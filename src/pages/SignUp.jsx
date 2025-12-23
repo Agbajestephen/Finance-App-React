bc0867cfede72274e2945214b7f2f4104b8c70a8
Agbajestephen <agbajestephen5@gmail.com>
2025-12-24 00:15:48 +0100

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -12,6 +12,7 @@ export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [loading, setLoading] = useState(false);
     e.preventDefault();
     
     if (!name || !email || !password || !confirmPassword) {
